dd40f1029091a14f75650e6af4508b7e00a49ca7
Abdul-Baseet Shabi <44884500+AbdulBaseetShabi@users.noreply.github.com>
2020-07-10 03:20:37 -0400

    using nodemon

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const routes = require('./routes');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-const host = process.env.HOST || 'localhost';
-const port = process.env.PORT || '3000';
+const host = process.env.host || 'localhost';
+const port = process.env.port || '3000';
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true,limit: '2gb'}));
