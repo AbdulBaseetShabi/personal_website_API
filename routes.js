bab208a7220f0b873b868c2e7984446dbc7fb630
Abdul-Baseet Shabi <44884500+AbdulBaseetShabi@users.noreply.github.com>
2020-07-10 03:19:46 -0400

    connection to mongodb

--- routes.js
+++ routes.js
@@ -1,2 +1,22 @@
+require('dotenv').config();
+const MongoClient = require('mongodb').MongoClient;
+const mongoDBConnectionString =  `mongodb+srv://${process.env.mongoDBUsername}:${process.env.mongoDBPassword}@cluster0-cthkj.mongodb.net/${process.env.db}?retryWrites=true&w=majority`
+var client = new MongoClient(mongoDBConnectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+
+(async ()=>{
+    try{
+        await client.connect(); 
+    }catch(err){
+        console.log(`MongoDB Error: ${err.message}`);
+    }finally{
+        if(client.isConnected()){
+            console.log('Connection established to DB')
+        }else{
+            console.log('Connection not established to DB')
+        }
+    }
+})();
+
+
 module.exports = {
 }
