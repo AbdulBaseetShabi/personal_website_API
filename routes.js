5f31cb82957ecff4d258e3f22b320765ef176ebb
Abdul-Baseet Shabi <44884500+AbdulBaseetShabi@users.noreply.github.com>
2020-07-10 06:02:23 -0400

    endpoint to test server, err handle, and get info

--- routes.js
+++ routes.js
@@ -3,6 +3,51 @@ const MongoClient = require('mongodb').MongoClient;
 const mongoDBConnectionString =  `mongodb+srv://${process.env.mongoDBUsername}:${process.env.mongoDBPassword}@cluster0-cthkj.mongodb.net/${process.env.db}?retryWrites=true&w=majority`
 var client = new MongoClient(mongoDBConnectionString, { useNewUrlParser: true, useUnifiedTopology: true });
 
+//GENERAL
+function testServer(req, res){
+    try {
+        res.status(200).send('Server is running...');   
+    } catch (err) {
+        console.log('Error');
+        res.status(500).send(err);
+    }
+}
+
+function endPointNotFound(req, res){
+    try{
+        res.status(404).send('Endpoint not found');
+    }catch (err) {
+        console.log('Error')
+        res.status(500).send(err);
+    }
+}
+
+//BIOGRAPHY
+async function getActiveBiography(req, res){
+    try {
+        await client.connect((err) =>{
+            if (err) throw err;
+            if(client.isConnected()){
+                console.log('Connection established to DB')
+                client.db('Resume').collection('biography').findOne({'active': true}, (err,result)=>{
+                    if (err) throw err; 
+                    res.status(200).send(result);
+                });
+            }else{
+                throw new Error('Connection not established to DB');
+            }
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    }finally{
+        client.close();
+    }
+    return;
+}
+
+//PROFILE
+//EXPERIENCE
 async function getExperience (req,res) {
     try {
         await client.connect((err) =>{
@@ -26,6 +71,35 @@ async function getExperience (req,res) {
     return;
 }
 
+//PROJECTS
+//CONTACT
+async function getContact (req, res) {
+    try {
+        await client.connect((err) =>{
+            if (err) throw err;
+            if(client.isConnected()){
+                console.log('Connection established to DB')
+                client.db('Resume').collection('contact').findOne({'active': true}, (err,result)=>{
+                    if (err) throw err; 
+                    res.status(200).send(result);
+                });
+            }else{
+                throw new Error('Connection not established to DB');
+            }
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    }finally{
+        client.close();
+    }
+    return;
+}
+
 module.exports = {
-    getExperience
+    testServer,
+    endPointNotFound,
+    getActiveBiography,
+    getExperience,
+    getContact
 }
